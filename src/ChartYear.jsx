b704a298842d0deb94728ac9523971e244e1ac45
eunmo <eunmo@cs.stanford.edu>
2022-01-21 06:51:49 +0000

    show missing

--- src/ChartYear.jsx
+++ src/ChartYear.jsx
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { Clear, Loupe } from '@mui/icons-material';
+import { Clear, Loupe, QuestionMark } from '@mui/icons-material';
 
 import { useStore } from './store';
 import { get } from './util';
 import ChartRows from './ChartRows';
-import Header from './Header';
+import Flag from './Flag';
+import Grid from './Grid';
 import WeekDialog from './WeekDialog';
 
 export default function ChartYear() {
   const [entries, setEntries] = useState(undefined);
   const [showButtons, setShowButtons] = useState(false);
+  const [showMissing, setShowMissing] = useState(false);
   const [openDialog, setOpenDialog] = useState(false);
   const { type, chart, year } = useParams();
   const store = useStore();
@@ -21,13 +23,34 @@ export default function ChartYear() {
     setEntries(undefined);
     setOpenDialog(false);
     setShowButtons(false);
+    setShowMissing(false);
     const url = `/api/chart/select/year/${type}/${chart}/${year}/10/${store}`;
     get(url, setEntries);
   }, [type, chart, year, store]);
 
+  const missing = useMemo(
+    () => (entries ?? []).filter(({ catalog, id }) => !catalog && id),
+    [entries]
+  );
+
+  const displayEntries = showMissing ? missing : entries;
+
   return (
     <>
-      <Header chart={chart}>
+      <Grid cols="1fr 50px auto 1fr" lh={50} mb={0}>
+        <div>
+          {missing && (
+            <IconButton
+              onClick={() => setShowMissing(!showMissing)}
+              size="large"
+            >
+              <QuestionMark />
+            </IconButton>
+          )}
+        </div>
+        <div>
+          <Flag chart={chart} />
+        </div>
         <div>
           <Button onClick={() => setOpenDialog(true)}>
             {year} Top 10 {type}s
@@ -38,11 +61,11 @@ export default function ChartYear() {
             {showButtons ? <Clear /> : <Loupe />}
           </IconButton>
         </div>
-      </Header>
+      </Grid>
       <ChartRows
         type={type}
         chart={chart}
-        entries={entries}
+        entries={displayEntries}
         showButtons={showButtons}
       />
       <WeekDialog
