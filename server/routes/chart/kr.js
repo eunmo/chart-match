3cf615354e7ed1b91b609929f6d8c77b8e72ccb3
eunmo <eunmo@cs.stanford.edu>
2022-07-08 14:58:23 +0000

    fix kr

--- server/routes/chart/kr.js
+++ server/routes/chart/kr.js
@@ -1,8 +1,8 @@
 const nodeFetch = require('node-fetch');
-const { getDoc, refDateWeek } = require('./util');
+const { refDateWeek } = require('./util');
 
 async function fetchAlbums(date) {
-  const [year, week] = refDateWeek(date, 0, 0);
+  const [year, week] = refDateWeek(date, 0, 6);
 
   const url = `https://circlechart.kr/data/api/chart/album`;
   const response = await nodeFetch(url, {
@@ -13,31 +13,33 @@ async function fetchAlbums(date) {
     },
   });
   const { List } = await response.json();
-  return Object.entries(List).map(([index, { ALBUM_NAME, ARTIST_NAME }]) => {
-    const rank = parseInt(index, 10) + 1;
-    const title = ALBUM_NAME.replace(/`/g, "'");
-    const artist = ARTIST_NAME.replace(/`/g, "'").replace(/\|.*$/, '').trim();
-    return { rank, artist, title };
-  });
+  return Object.entries(List ?? {})
+    .slice(0, 100)
+    .map(([index, { ALBUM_NAME, ARTIST_NAME }]) => {
+      const rank = parseInt(index, 10) + 1;
+      const title = ALBUM_NAME.replace(/`/g, "'");
+      const artist = ARTIST_NAME.replace(/`/g, "'").replace(/\|.*$/, '').trim();
+      return { rank, artist, title };
+    });
 }
 
 async function fetchSingles(date) {
-  const [year, week] = refDateWeek(date, 0, 0);
+  const [year, week] = refDateWeek(date, 0, 6);
 
   const url = `https://circlechart.kr/data/api/chart/onoff`;
   const response = await nodeFetch(url, {
     method: 'post',
-    body: `nationGbn=T&termGbn=week&hitYear=${year}&targetTime=${week}&yearTime=3&curUrl=circlechart.kr%2Fpage_chart%2Falbum.onoff%3F`,
+    body: `nationGbn=T&serviceGbn=ALL&termGbn=week&hitYear=${year}&targetTime=${week}&yearTime=3&curUrl=circlechart.kr%2Fpage_chart%2Fonoff.circle%3FserviceGbn%3DALL`,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     },
   });
   const { List } = await response.json();
-  return Object.entries(List)
+  return Object.entries(List ?? {})
     .slice(0, 100)
-    .map(([index, { ALBUM_NAME, ARTIST_NAME }]) => {
+    .map(([index, { SONG_NAME, ARTIST_NAME }]) => {
       const rank = parseInt(index, 10) + 1;
-      const title = ALBUM_NAME.replace(/`/g, "'");
+      const title = SONG_NAME.replace(/`/g, "'");
       const artist = ARTIST_NAME.replace(/`/g, "'").replace(/\|.*$/, '').trim();
       return { rank, artist, title };
     });
